5ad357140048634222b46531b135c9e363c097c0
Artur Grigorev <agrigorev@ethz.ch>
2025-04-09 15:12:25 +0200

    defaults.py cleaned

--- utils/defaults.py
+++ utils/defaults.py
@@ -7,48 +7,14 @@ hostname = socket.gethostname()
 
 DEFAULTS = dict()
 
-DEFAULTS['project_name'] = 'impulsehood_unimodel'
-
-if hostname == 'ohws68.inf.ethz.ch':
-    HOOD_PROJECT = "/local/home/agrigorev/Workdir/00_Projects/contourcraft_private"
-    HOOD_DATA = "/local/home/agrigorev/Data/02_Projects/ccraft_data"
-
-    DEFAULTS['server'] = 'stdpc'
-    DEFAULTS['CMU_root'] = '/local/home/agrigorev/Data/00_Datasets/AMASS/smpl/CMU'
-    DEFAULTS['data_root'] = HOOD_DATA
-    DEFAULTS['aux_data'] = os.path.join(HOOD_DATA, 'aux_data')
-    DEFAULTS['project_dir'] = HOOD_PROJECT
-    DEFAULTS['experiment_root'] = "/media/sdb/Data/experiments/"
-elif hostname == 'ait-server-04.inf.ethz.ch':
-    HOOD_PROJECT = "/local/home/agrigorev/Workdir/contourcraft_private"
-    HOOD_DATA = "/data/agrigorev/02_Projects/ccraft_data"
-    DEFAULTS['server'] = 'server4'
-    DEFAULTS['CMU_root'] = '/data/agrigorev/00_Datasets/AMASS/smpl/CMU'
-    DEFAULTS['data_root'] = HOOD_DATA
-    DEFAULTS['aux_data'] = os.path.join(HOOD_DATA, 'aux_data')
-    DEFAULTS['project_dir'] = HOOD_PROJECT
-    DEFAULTS['experiment_root'] = "/data/agrigorev/experiments/"
-    pass
-elif hostname.startswith('g'):
-    HOOD_PROJECT = "/lustre/home/agrigorev/Workdir/contourcraft_private"
-    HOOD_DATA = '/lustre/fast/fast/agrigorev/02_Projects/ccraft_data'
-    DEFAULTS['server'] = 'mpi'
-    DEFAULTS['CMU_root'] = '/is/cluster/fast/agrigorev/Data/AMASS/smpl/CMU'
-    DEFAULTS['data_root'] = '/lustre/fast/fast/agrigorev/02_Projects/ccraft_data'
-    DEFAULTS['aux_data'] = os.path.join(DEFAULTS['data_root'], 'aux_data')
-    DEFAULTS['project_dir'] = "/lustre/home/agrigorev/Workdir/contourcraft_private"
-    DEFAULTS['experiment_root'] = os.path.join(DEFAULTS['data_root'], 'experiments')
-else:
-    HOOD_PROJECT = os.environ["HOOD_PROJECT"]
-    HOOD_DATA = os.environ["HOOD_DATA"]
+DEFAULTS['project_name'] = 'ccraft'
 
 
-    DEFAULTS['server'] = 'local'
-    DEFAULTS['data_root'] = HOOD_DATA
-    DEFAULTS['experiment_root'] = os.path.join(HOOD_DATA, 'experiments')
-    DEFAULTS['aux_data'] = os.path.join(HOOD_DATA, 'aux_data')
-    DEFAULTS['project_dir'] = HOOD_PROJECT
+DEFAULTS['CMU_root'] = '/path/to/AMASS/smpl/CMU'
+DEFAULTS['data_root'] = '/path/to/ccraft_data'
+DEFAULTS['aux_data'] = os.path.join(DEFAULTS['data_root'], 'aux_data')
+DEFAULTS['project_dir'] = 'path/to/this/repo'
+DEFAULTS['experiment_root'] = os.path.join(DEFAULTS['data_root'], 'experiments')
 
 
-DEFAULTS['hostname'] = hostname
 DEFAULTS = munchify(DEFAULTS)
