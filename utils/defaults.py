8f94129e45fe12f51ea30d26fdbf03e5aa125e7d
Artur Grigorev <agrigorev@ethz.ch>
2025-05-06 19:43:56 +0200

    cleanup

--- utils/defaults.py
+++ utils/defaults.py
@@ -10,16 +10,10 @@ DEFAULTS = dict()
 DEFAULTS['project_name'] = 'ccraft'
 
 
-# DEFAULTS['CMU_root'] = '/path/to/AMASS/smpl/CMU'
-# DEFAULTS['data_root'] = '/path/to/ccraft_data'
-# DEFAULTS['aux_data'] = os.path.join(DEFAULTS['data_root'], 'aux_data')
-# DEFAULTS['project_dir'] = 'path/to/this/repo'
-# DEFAULTS['experiment_root'] = os.path.join(DEFAULTS['data_root'], 'experiments')
-
-DEFAULTS['CMU_root'] = ''
-DEFAULTS['data_root'] = '/data/agrigorev/02_Projects/ccraft_data'
+DEFAULTS['CMU_root'] = '/path/to/AMASS/smpl/CMU'
+DEFAULTS['data_root'] = '/path/to/ccraft_data'
 DEFAULTS['aux_data'] = os.path.join(DEFAULTS['data_root'], 'aux_data')
-DEFAULTS['project_dir'] = '/local/home/agrigorev/Workdir/contourcraft_private'
+DEFAULTS['project_dir'] = 'path/to/this/repo'
 DEFAULTS['experiment_root'] = os.path.join(DEFAULTS['data_root'], 'experiments')
 
 DEFAULTS = munchify(DEFAULTS)
